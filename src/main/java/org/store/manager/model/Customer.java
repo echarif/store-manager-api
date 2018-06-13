17fcc05c6ebe5788de69d9cac96b79539baa7b06
echarif <hasanecharif@gmail.com>
2018-06-13 22:00:08 +0200

    Set customer fields as NotBlank

--- src/main/java/org/store/manager/model/Customer.java
+++ src/main/java/org/store/manager/model/Customer.java
@@ -2,6 +2,7 @@ package org.store.manager.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
 
@@ -14,10 +15,20 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+
+	@NotBlank
 	private String name;
+
+	@NotBlank
 	private String surname;
+
+	@NotBlank
 	private String mobilePhone;
+
+	@NotBlank
 	private String phone;
+
+	@NotBlank
 	private String email;
 
 	public Customer() {
